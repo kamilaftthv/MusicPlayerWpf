48679df07c70d36a7f517020e86d1ad2b8e4b1c4
Камила Фаттахова <kamilafattahova65210@gmail.com>
2024-06-14 21:33:57 +0300

    a little optimization

--- MusicPlayerWpf/MainWindow.xaml.cs
+++ MusicPlayerWpf/MainWindow.xaml.cs
@@ -341,21 +341,4 @@ namespace MusicPlayerWpf
             ChangeCursor(_normalCursor);
         }
     }
-
-    public class TrackInfo
-    {
-        public string FilePath { get; set; }
-        public string Name { get; set; }
-        public string Artist { get; set; }
-        public string Duration { get; set; }
-        public BitmapImage AlbumCover { get; set; }
-        public TrackInfo()
-        {
-            FilePath = string.Empty;
-            Name = string.Empty;
-            Artist = string.Empty;
-            Duration = string.Empty;
-            AlbumCover = null;
-        }
-    }
 }
---
+++ MusicPlayerWpf/TrackInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace MusicPlayerWpf
+{
+    public class TrackInfo
+    {
+        public string FilePath { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Duration { get; set; }
+        public BitmapImage AlbumCover { get; set; }
+        public TrackInfo()
+        {
+            FilePath = string.Empty;
+            Name = string.Empty;
+            Artist = string.Empty;
+            Duration = string.Empty;
+            AlbumCover = null;
+        }
+    }
+}
