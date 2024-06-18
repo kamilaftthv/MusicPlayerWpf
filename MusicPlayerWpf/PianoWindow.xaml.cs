8b815e83b704181d8b1c51b7ae9b3b804637fa4c
Камила Фаттахова <kamilafattahova65210@gmail.com>
2024-06-19 00:36:19 +0300

    correct piano

--- MusicPlayerWpf/PianoWindow.xaml.cs
+++ MusicPlayerWpf/PianoWindow.xaml.cs
@@ -19,7 +19,6 @@ namespace MusicPlayerWpf
     /// </summary>
     public partial class PianoWindow : Window
     {
-        Random rnd = new Random();
         public PianoWindow()
         {
             InitializeComponent();
@@ -27,37 +26,65 @@ namespace MusicPlayerWpf
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Console.Beep(rnd.Next(37, 1000), rnd.Next(37, 1000));
+            Console.Beep(261, 500);
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            Console.Beep(rnd.Next(37, 1000), rnd.Next(37, 1000));
+            Console.Beep(293, 500);
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            Console.Beep(rnd.Next(37, 1000), rnd.Next(37, 1000));
+            Console.Beep(329, 500);
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-            Console.Beep(rnd.Next(37, 1000), rnd.Next(37, 1000));
+            Console.Beep(349, 500);
         }
 
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
-            Console.Beep(rnd.Next(37, 1000), rnd.Next(37, 1000));
+            Console.Beep(392, 500);
         }
 
         private void Button_Click_5(object sender, RoutedEventArgs e)
         {
-            Console.Beep(rnd.Next(37, 1000), rnd.Next(37, 1000));
+            Console.Beep(440, 500);
         }
 
         private void Button_Click_6(object sender, RoutedEventArgs e)
         {
-            Console.Beep(rnd.Next(37, 1000), rnd.Next(37, 1000));
+            Console.Beep(493, 500);
         }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.D:
+                    Button_Click(ButtonC, null);
+                    break;
+                case Key.F:
+                    Button_Click_1(ButtonD, null);
+                    break;
+                case Key.G:
+                    Button_Click_2(ButtonE, null);
+                    break;
+                case Key.H:
+                    Button_Click_3(ButtonF, null);
+                    break;
+                case Key.J:
+                    Button_Click_4(ButtonG, null);
+                    break;
+                case Key.K:
+                    Button_Click_5(ButtonA, null);
+                    break;
+                case Key.L:
+                    Button_Click_6(ButtonB, null);
+                    break;
+            }
+        }
+
     }
 }
